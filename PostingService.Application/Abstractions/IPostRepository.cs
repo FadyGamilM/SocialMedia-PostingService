6d92544922f44ec406fbaa1b3b41e04df14f2d3a
fady <gamilfady605@gmail.com>
2022-11-28 22:40:17 +0200

    port between data access layer and application layer

---
+++ PostingService.Application/Abstractions/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using PostingService.Domain.Entities;
+
+namespace PostingService.Application.Abstractions;
+public interface IPostRepository
+{
+    Task<IEnumerable<Post>> GetPosts();
+
+    Task<Post> GetPostById();
+
+    Task<Post> CreatePost(Post post);
+
+    Task<Post> UpdatePost(string updatedContent, int postId);
+
+    Task DeletePost(int postId);
+}
