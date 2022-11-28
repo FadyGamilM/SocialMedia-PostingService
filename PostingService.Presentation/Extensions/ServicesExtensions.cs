587fe915381fb10c62c00185fa21a9ee387aa5b2
fady <gamilfady605@gmail.com>
2022-11-28 23:58:18 +0200

    first migrations

---
+++ PostingService.Presentation/Extensions/ServicesExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostingService.Application.Abstractions;
+using PostingService.DataAccess;
+using PostingService.DataAccess.Repositories;
+
+namespace PostingService.Presentation.Extensions;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        // register db context
+        builder.Services.AddDbContext<PostingServiceDbContext>(opts => 
+            opts.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                )
+        );
+
+        // inject post repository service
+        builder.Services.AddScoped<IPostRepository, PostRepository>();
+        return services;
+    }
+}
--- PostingService.Presentation/Program.cs
+++ PostingService.Presentation/Program.cs
@@ -1,6 +1,11 @@
+using PostingService.Presentation.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// 1)==> register the database context
+builder.Services.RegisterServices(builder);
 
 var app = builder.Build();
 
@@ -8,27 +13,5 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
